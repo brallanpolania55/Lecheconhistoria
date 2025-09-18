930aa69d767adc85af48f7ec022a6bc5e38fcd74
Lecheconhistoria <brallanpolania55@gmail.com>
2025-09-17 19:45:39 -0500

    Add files via upload

---
+++ game.js
@@ -0,0 +1,13 @@
+document.querySelector('form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Formulario enviado correctamente');
+});
+
+
+function redirigirProducto() {
+  window.location.href = "https://www.tusitio.com/productos";
+}
+
+function redirigirProducto() {
+  window.location.href = "https://www.tusitio.com/productos";
+}
